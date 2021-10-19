6bd1dc756bd1f534c16d0642fecb31fc16081b0d
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-10-19 21:37:09 +0200

    counts how many of some letter string contains

---
+++ 3.10/Open-Lab-03.10/Checker.cs
@@ -0,0 +1,26 @@
+﻿namespace Open_Lab_03._10
+{
+    public class Checker
+    {
+        public int GetNumberOfCharsInString(char letter, string str)
+        {
+            int i = 0;
+            foreach(char c in str)
+            {
+                if (c == letter)
+                {
+                    i++;
+                }
+                else
+                {
+
+                }
+
+            }
+
+            return i;
+
+
+        }
+    }
+}
